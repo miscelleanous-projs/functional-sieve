673d24abf76e9c35bf9f9e0714a204c91ec6219d
Menjanahary <menjaraz@gmail.com>
2023-12-28 17:26:48 +0300

    Add Project

---
+++ rust-version/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    // closure syntax
+    let is_prime = |number: usize| -> bool {
+        number >= 2 && 
+            (number == 2 || number == 3 ||
+            (number % 2 != 0 && number % 3 != 0 &&
+            !(5..=(number as f64).sqrt() as usize)
+            .any(|x| number % x == 0))
+        )
+    };
+
+    let primes: Vec<usize> = (2..5_000).filter(|&x| is_prime(x)).collect();
+
+    println!("{:?}", primes);
+}
